88e3b4c54cf2b35886f91bc3424bd389e32924b0
magentaFire <dafne.magenta@gmail.com>
2019-06-07 02:31:16 -0500

    created components, WIP-create todos

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+export default class TodosList extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    Welcome to To Do's list component
+                </p>
+            </div>
+        )
+    }
+}
