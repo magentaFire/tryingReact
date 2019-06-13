d2fa5e7dfebb3dc18a601673bccc88992149a0dd
magentaFire <dafne.magenta@gmail.com>
2019-06-13 01:13:50 -0500

    3rd part completed

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component } from 'react'
 export default class CreateToDo extends Component {
     constructor(props) {
@@ -32,10 +33,15 @@ export default class CreateToDo extends Component {
     }
     onSubmit(e) {
         e.preventDefault();
-        console.log('Form submitted');
-        console.log(`Todo Description ${this.state.todo_description}`);
-        console.log(`Todo Responsible ${this.state.todo_responsible}`);
-        console.log(`Todo Priority ${this.state.todo_priority}`);
+        const newTodo = {
+            todo_description: this.state.todo_description,
+            todo_responsible: this.state.todo_responsible,
+            todo_priority: this.state.todo_priority,
+            todo_completed: this.state.todo_completed
+        }
+        axios.post('http://localhost:4000/todos/add', newTodo)
+            .then(res => console.log(res.data))
+
         this.setState({
             todo_description: '',
             todo_responsible: '',
--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Todo = props => (
+    <tr>
+        <td>{ props.todo.todo_description }</td>
+        <td>{ props.todo.todo_responsible }</td>
+        <td>{ props.todo.todo_priority }</td>
+        <td>
+            <Link to={"/edit/" + props.todo._id}>Edit</Link>
+        </td>
+    </tr>
+)
+
 export default class TodosList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/todos/')
+            .then(response => {
+                this.setState({ todos: response.data })
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
+    todoList() {
+        return this.state.todos.map((currentTodo, i) => {
+            return <Todo todo={currentTodo} key={i} />
+        })
+    }
+
     render() {
         return (
             <div>
-                <p>
-                    Welcome to To Do's list component
-                </p>
+                <h3>To Do's List</h3>
+                <table className="table table-striped" style={{ marginTop:20 }}>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Responsible</th>
+                            <th>Priority</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.todoList() }
+                    </tbody>
+                </table>
             </div>
         )
     }
